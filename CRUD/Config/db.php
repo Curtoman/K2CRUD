6c3bcadd3633fde350e9eb8af3f475f4a5402702
Curtoman <84277755+Curtoman@users.noreply.github.com>
2021-05-16 16:01:53 +0200

    Add files via upload

---
+++ CRUD/Config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$databaseHost = 'localhost';
+$databaseName = 'simplecrud';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+?>
